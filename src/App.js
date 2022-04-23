d17274a40397d7edcee366c82cb1ec17fafe4348
Hariskhan2 <89725754+Hariskhan2@users.noreply.github.com>
2022-04-23 16:16:43 +0500

    Calling an Api to fetch data

--- src/App.js
+++ src/App.js
@@ -2,6 +2,20 @@
 import './App.css';
 
 function App() {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country, //UnitedStates,United Kindom
+            value: country.countryInfo.iso3, //USA,UK
+          }));
+          setCountries(countries);
+        });
+    };
+
+    getCountriesData();
+  }, []);
   return (
     <div className="App">
       
