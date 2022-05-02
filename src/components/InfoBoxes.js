256c1efa5535440d2463d97e288d2b712468b65a
Hariskhan2 <89725754+Hariskhan2@users.noreply.github.com>
2022-05-02 15:10:47 +0500

    Update the infoboxes
    it is to  change the text color of infoboxes.It takes colors as a props from app.js and then styled in the infobox.css file

--- src/components/InfoBoxes.js
+++ src/components/InfoBoxes.js
@@ -8,6 +8,18 @@ function InfoBoxes(props) {
         <Typography className='infoBox_title' color="textSecondary">{props.title}</Typography>
         <h2 className='infoBox_cases'>{props.cases}</h2>
         <Typography className='infoBox_total' color='textSecondary'>{props.total} Total</Typography>
+          {props.title}
+        </Typography>
+        <h2
+          className={`infoBox_cases ${props.isRed && "infoBox_Red"} ${
+            !(props.isRed || props.isBlue) && "infoBox_cases_green"
+          } ${!(props.isRed || props.isGreen) && "infoBox_cases_blue"} `}
+        >
+          {props.cases}
+        </h2>
+        <Typography className="infoBox_total" color="textSecondary">
+          {props.total} Total
+        </Typography>
       </CardContent>
     </Card>
   );
