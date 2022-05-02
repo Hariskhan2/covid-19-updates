6bb4e025cfdd22b28926b8391d960490b24a82a1
Hariskhan2 <89725754+Hariskhan2@users.noreply.github.com>
2022-05-02 15:14:32 +0500

    update Util.js
    it is to design  the popup card when we click on a particular country and also to show the circles on the map to represents the numbers of cases ,recovered,deaths

--- src/util.js
+++ src/util.js
@@ -9,3 +9,39 @@ export const sortData = (data) => {
   });
   return sortedData;
 };
+export const prettyPrintStat = (stat) =>
+  stat ? `+${numeral(stat).format("0.0a")}` : "0a";
+//Draw circles on map
+export const showDataOnMap = (data, casesType = "cases") =>
+  data.map((country) => (
+    <Circle
+      center={[country.countryInfo.lat, country.countryInfo.long]}
+      fillOpacity={0.4}
+      color={casesTypeColors[casesType].hex}
+      fillColor={casesTypeColors[casesType].hex}
+      radius={
+        Math.sqrt(
+          Math.sqrt(country[casesType]) * casesTypeColors[casesType].multiplier
+        ) * casesTypeColors[casesType].multiplier
+      }
+    >
+      <Popup>
+        <div className="info-container">
+          <div
+            className="info-flag"
+            style={{ backgroundImage: `url(${country.countryInfo.flag})` }}
+          ></div>
+          <div className="info-name">{country.country}</div>
+          <div className="info-confirmed">
+            Cases:{numeral(country.cases).format("0,0")}
+          </div>
+          <div className="info-recovered">
+            Recovered:{numeral(country.recovered).format("0,0")}
+          </div>
+          <div className="info-deaths">
+            Deaths:{numeral(country.deaths).format("0,0")}
+          </div>
+        </div>
+      </Popup>
+    </Circle>
+  ));
